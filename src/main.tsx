5b66fcc37904c2d1a96335131079d9f09113a345
Abdul Majeed Kanoor <abdulkanoor@gmail.com>
2025-07-27 14:37:46 +0530

    fix: ensure Bootstrap CSS and JS are imported for proper styling and functionality

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import App from './App.tsx';
 import { Provider } from 'react-redux';
 import { store } from './redux/store.ts';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
